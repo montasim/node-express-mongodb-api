f4f4c98b8ad0fd8707aabe3df3f082a31f230375
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-03-13 08:41:56 +0600

    get all users data

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,22 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 24,
+  },
+];
+
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
-  res.send('users');
+  res.send(users);
 });
 
 export default router;
