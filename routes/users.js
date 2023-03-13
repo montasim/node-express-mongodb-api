a5aa2584269fed9e465184ca214ad4d9459eb458
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-03-13 22:33:47 +0600

    add user

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,21 @@ router.get('/', (req, res) => {
   res.send(usersData);
 });
 
+router.post('/', (req, res) => {
+  const { firstName, lastName, age } = req.body;
+  const newUser = { firstName, lastName, age };
+
+  usersData.push(newUser); // add the new user to the existing array
+
+  // write the updated data to the JSON file
+  fs.writeFile('../user.json', JSON.stringify(usersData), (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error writing to file');
+    } else {
+      res.status(200).send('User added successfully');
+    }
+  });
+});
+
 export default router;
