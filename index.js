5a3bd0d5fdf6eccb3e80af925884b798cc96784a
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-03-13 08:32:04 +0600

    users route

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 // const express = require('express'); // equivalent to previous lin
 import bodyParser from 'body-parser';
+import usersRoutes from './routes/users.js';
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+
+// users routes
+app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => res.send(`Hello from Homepage.`));
 
